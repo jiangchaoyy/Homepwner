a8a2ce5491657c94e60c9c3d9922cac4a79afc8f
jiangchaoyy <jiangchaoyy@yahoo.com>
2015-08-28 07:24:20 +0800

    Init - add

---
+++ Homepwner/Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by jiangchao on 15/8/28.
+//  Copyright (c) 2015年 jiangchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/Homepwner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepwner
+//
+//  Created by jiangchao on 15/8/28.
+//  Copyright (c) 2015年 jiangchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
